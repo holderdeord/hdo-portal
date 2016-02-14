8f6ae86d92fbfbca9c5f35febc33eb16c51bc3ba
Jari Bakken <jari@holderdeord.no>
2016-02-14 15:28:44 +0100

    More ES6isms

--- server/server.js
+++ server/server.js
@@ -35,11 +35,11 @@ app.get('/robots.txt', function(req, res) {
 
 var oldPaths = /^\/(admin|api|categories|committees|districts|docs|healthz|home|info|issues|parliament-issues|parties|promises|propositions|questions|representative|representatives|search|users|votes|widgets).*/
 app.get(oldPaths, function(req, res) {
-    res.redirect(`https://data.holderdeord.no${req.path}`)
+    res.redirect('https://data.holderdeord.no' + req.path)
 });
 
 var port = +(process.env.HTTP_PORT || 3000);
 
 app.listen(port, function() {
-    console.log(`hdo-portal is listening on port ${port}`);
+    console.log('hdo-portal is listening on port ' + port);
 });
