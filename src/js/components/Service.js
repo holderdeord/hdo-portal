6b3c773804cf3f545523b776f47af79abe6bf34c
Jari Bakken <jari@holderdeord.no>
2016-02-17 00:29:20 +0100

    Fix selection color

---
+++ src/js/components/Service.js
@@ -0,0 +1,15 @@
+export defatul (service) => (
+    <div className="service hdo-card m-y-1">
+        <a href={service.url}>
+            <div
+                className="img"
+                style={service.style || {backgroundImage: `url(${service.img})`}} />
+
+            <div className="p-a-1">
+                <h4>{service.title}</h4>
+                <p className="description">{service.description}</p>
+            </div>
+        </a>
+    </div>
+);
+
--- src/js/components/Services.js
+++ src/js/components/Services.js
@@ -26,18 +26,7 @@ export default class Services extends Component {
                     <div className="row" key={serviceGroup.map(e => e.title).join()}>
                         {serviceGroup.map(service => (
                             <div className="col-lg-6" key={service.title}>
-                                <div className="service hdo-card m-y-1">
-                                    <a href={service.url}>
-                                        <div
-                                            className="img"
-                                            style={service.style || {backgroundImage: `url(${service.img})`}} />
-
-                                        <div className="p-a-1">
-                                            <h4>{service.title}</h4>
-                                            <p className="description">{service.description}</p>
-                                        </div>
-                                    </a>
-                                </div>
+                                <Service {...service} />
                             </div>
                         ))}
                     </div>
